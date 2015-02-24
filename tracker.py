771e516c59558b8975b4e00d91685c6c10afcf31
Steve <stephan.fabregas@gmail.com>
2015-02-23 21:43:15 -0500

    Broke out some basic functionality.

--- tracker.py
+++ tracker.py
@@ -1,6 +1,52 @@
 from Tkinter import Tk, RIGHT, BOTH, RAISED
 from ttk import Frame, Button, Style, Canvas
 
+class Display(Canvas):
+
+    def __init__(self, parent):
+        Canvas.__init__(self)
+
+        self.parent = parent
+        self.initDisplay()
+        self.pack()
+
+    def initDisplay(self):
+        self.timer = 30
+        self.state = "NA"
+
+        self.drawStatus()
+        self.bind_all("<Key>", self.onKeyPressed)
+
+    def drawStatus(self):
+        self.create_text(20, 30, anchor=W, text=self.state)
+
+    def onKeyPressed(self, e):
+        key = e.keysym
+
+        if key == "Left":
+            self.state = "Left"
+
+        if key == "Right":
+            self.state = "Right"
+
+        if key == "Up":
+            self.state= = "Both"
+
+        if key == "Down":
+            self.state = "Neither"
+
+    def onTimer(self):
+        self.drawStatus()
+        self.after(DELAY, self.onTimer)
+
+    def onEpoch(self):
+        self.updateFile()
+        # Open file, write last epoch, close file
+        # Consider event handlers...
+        self.after(EPOCH, self.onEpoch)
+
+    def updateFile(self):
+
 class Tracker(Frame):
 
     def __init__(self, parent):
@@ -26,8 +72,8 @@ class Tracker(Frame):
         self.rowconfigure(1, pad=3)
         self.rowconfigure(2, pad=3)
 
-        # No canvas, just have readout of current state and previous state
-        display = Canvas(self)
+        # Canvas to read out current state and previous state
+        self.display = Display(parent)
         display.grid(row=0, columnspan=2, sticky=W+E)
         #
 
