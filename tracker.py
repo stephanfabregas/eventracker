a5de44ec606e3e9333f7fd34d26d8bd70f04dcf5
Steve <stephan.fabregas@gmail.com>
2015-02-24 20:27:42 -0500

    Working on logic

--- tracker.py
+++ tracker.py
@@ -4,25 +4,34 @@ from ttk import Frame, Button, Style, Canvas
 class Display(Canvas):
 
     def __init__(self, parent):
-        Canvas.__init__(self)
+        super(Display, self).__init__()
 
         self.parent = parent
         self.initDisplay()
         self.pack()
 
     def initDisplay(self):
-        self.timer = 30
+        self.epoch = 0
+        self.savedEpoch = 0
+        # self.startTime
+        # self.currentTime
+
         self.state = "NA"
 
         self.drawStatus()
         self.bind_all("<Key>", self.onKeyPressed)
+        # bind the button press events as well
+
+        self.after(DELAY, self.onTimer())
 
     def drawStatus(self):
         self.create_text(20, 30, anchor=W, text=self.state)
 
     def onKeyPressed(self, e):
+        # Check if in new epoch, checkEpoch method
         key = e.keysym
 
+        # Update state (maybe a setState method)
         if key == "Left":
             self.state = "Left"
 
@@ -35,22 +44,38 @@ class Display(Canvas):
         if key == "Down":
             self.state = "Neither"
 
-    def onTimer(self):
-        self.drawStatus()
-        self.after(DELAY, self.onTimer)
+        self.onEvent()
 
-    def onEpoch(self):
-        self.updateFile()
+    def onEvent(self):
+        # Use self.state to update the canvas
+        self.drawStatus()
+
+    def onTimer(self):
         # Open file, write last epoch, close file
-        # Consider event handlers...
-        self.after(EPOCH, self.onEpoch)
+        # Maybe not first... check logic
+        self.updateFile()
+        # Need to figure out how to prioritize this event...
+        # Track system time to check current Epoch #,
+        # Update self.currentTime
+        # Update self.epoch
+        # For range(checkEpoch (# unsaved epochs) - 1):
+            # Update epoch
+            # UpdateFile with NA
+            # If not, 
 
     def updateFile(self):
+        # Open file
+        # Write/append current epoch
+        # Update last written epoch: self.savedEpoch
+        # Close file
+
+    def checkEpoch(self):
+        # return self.epoch-self.savedEpoch
 
 class Tracker(Frame):
 
     def __init__(self, parent):
-        Frame.__init__(self, parent)
+        super(Tracker, self).__init__(parent)
 
         self.parent = parent
         self.initUI()
